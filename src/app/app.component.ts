61807b4210a7f76b6a4cf658b61b2362c48ccceb
Carlos Ramírez <calbertora@gmail.com>
2019-10-22 15:29:45 -0500

    Add the service and the get method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDoServiceService } from './to-do-service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TodoApp';
+  task: string;
+  tasks: any;
+
+  constructor(private todoService: ToDoServiceService) {
+    this.task = '';
+    this.tasks = [];
+    this.getTask();
+  }
+
+  getTask() {
+    this.todoService.getTasks().subscribe(response => this.tasks = response);
+  }
+  setTask() {
+
+  }
 }
---
+++ src/app/to-do-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToDoServiceService {
+
+  constructor( private httpClient: HttpClient) { }
+
+  getTasks() {
+    const url = `${environment.endpoint}Task`;
+    return this.httpClient.get(url);
+  }
+}
